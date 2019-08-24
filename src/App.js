06ab4368bbb03f4dab8a2841b65944ab782020fd
Patrick Battisti <1729patrick@gmail.com>
2019-08-24 01:04:44 -0300

    Carregamento do tema pela api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import axios from 'axios';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
-
+//
 const App = () => {
+  const [theme, setTheme] = useState();
+
+  useEffect(() => {
+    const load = async () => {
+      const sessions = await axios.get('http://localhost:3333/sessions', {
+        headers: { domain: 'kkkaasjndasjdn' },
+      });
+
+      const themex = await axios.get(
+        `http://localhost:3333/static/core/colors/${sessions.data.company._id}.json`
+      );
+
+      setTheme(themex.data);
+    };
+
+    load();
+  }, []);
+
+  if (!theme) {
+    return <div>carregando</div>;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <>
